1695ec3a9b69429a08ac54926e963f0ca1d40a99
Khrunev Denis <deniskhrunev@gmail.com>
2024-08-08 12:36:16 +0600

    Создана структура POST запросов на Decode и Encode

--- cipher-api/routes/ciphers.ts
+++ cipher-api/routes/ciphers.ts
@@ -1,6 +1,24 @@
 import express from 'express';
-
+import { VigenereCipher } from 'vigenere.js';
+import {Decode, Encode} from '../types';
 
 const ciphersRouter = express.Router();
+const vigenere = new VigenereCipher();
+
+ciphersRouter.post('/encode', (req, res) => {
+  const encode: Encode = {
+    encoded: vigenere.encrypt(req.body.message, req.body.password).toLowerCase(),
+  };
+
+  return res.send(encode);
+});
+
+ciphersRouter.post('/decode',  (req, res) => {
+  const decode: Decode = {
+    decoded: vigenere.decrypt(req.body.message, req.body.password).toLowerCase(),
+  };
+
+  return res.send(decode);
+});
 
 export default ciphersRouter;
