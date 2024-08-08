481915ee6cab390b291ad3d43110752b2e2bd241
Khrunev Denis <deniskhrunev@gmail.com>
2024-08-08 13:26:06 +0600

    Создана структура для работы с глобальным стейтом

--- cipher-frontend/src/App.tsx
+++ cipher-frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import {Container, Typography} from '@mui/material';
+import {Route, Routes} from 'react-router-dom';
 
 const App = () => (
   <>
+    <header>
 
+    </header>
+    <Container maxWidth='xl' component='main'>
+      <Routes>
+        <Route path='*' element={<Typography variant='h1'>Not found</Typography>} />
+      </Routes>
+    </Container>
   </>
 );
 
--- cipher-frontend/src/main.tsx
+++ cipher-frontend/src/main.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {store} from './app/store';
+import {Provider} from 'react-redux';
+import theme from './theme';
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>,
 );
